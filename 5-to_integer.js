98e20d521f19e00fd0b6a8a3ba6772d6c0ced297
Itumeleng Seoane <tumynaxy@gmail.com>
2025-09-14 19:27:45 +0200

    Add 5-to_integer.js to handle integer arguments

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,13 +3,10 @@
 // Get the first argument
 const arg = process.argv[2];
 
-// Convert it to an integer
-const num = parseInt(arg, 10);
-
-// Check if the conversion produced a valid number
-if (!isNaN(num)) {
+// Check if the argument exists and can be converted to an integer
+if (arg && !isNaN(parseInt(arg, 10))) {
+    const num = parseInt(arg, 10);
     console.log(`My number: ${num}`);
 } else {
     console.log("Not a number");
 }
-x
