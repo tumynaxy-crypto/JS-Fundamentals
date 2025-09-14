b4622d8444f29f7450deabd1339a809903ac35e6
Itumeleng Seoane <tumynaxy@gmail.com>
2025-09-14 20:26:29 +0200

    Add 7-multi_c.js to print 'C is fun' x times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,16 +1,11 @@
 // 7-multi_c.js
 
-// Get the first argument
 const arg = process.argv[2];
-
-// Convert to integer
 const times = parseInt(arg, 10);
 
-// Check if the argument is a valid positive number
 if (!isNaN(times) && times > 0) {
     for (let i = 0; i < times; i++) {
         console.log("C is fun");
     }
-} else {
-    console.log("Missing number of occurrences");
 }
+// If times is missing, invalid, or <= 0, do nothingx
